e159bf173e0a60f9566d589829675da82daead47
Ravi Kumar <p.ravi.singh292@gmail.com>
2021-04-24 12:30:38 +0530

    Details till product detail added

---
+++ angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product 
+{
+    id: String;
+    sku: String;
+    name: String;
+    description: String;
+    unitPrice: number;
+    imageUrl: String;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
